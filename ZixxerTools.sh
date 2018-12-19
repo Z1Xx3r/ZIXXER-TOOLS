dd2487316b46c84ebd2185649724d3dacbac919f
Z1Xx3r <45943456+Z1Xx3r@users.noreply.github.com>
2018-12-19 10:54:06 +0800

    ZIXXER-TOOLS

---
+++ ZixxerTools.sh
@@ -0,0 +1,308 @@
+#MAU NGAPAIN LUU NGENTOD?:V
+#RECODE? LU PIKIR DENGAN RECODE LU BISA JADI MASTAH?
+#COPAS? PIKIR SENDIRI KONTOL HARGAI USAHA ORANG LAIN DONG
+#GUA SUMPAHIN YANG RECODE, COPAS, KELUARGANYA NGANGGUR 7 TURUNAN
+#PIKIR SENDIRI NGENTOD!!!
+#MALU GOBLOKK MALUU!!
+
+#19-12-2018
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+clear
+
+figlet ZIXXER TOOLS | lolcat
+
+sleep 1
+
+echo $red"           ▒▒▒   ▒▒▒   ▒▒▒   ▒▒▒   ▒▒▒   ▒▒▒   ▒▒▒      "   |lolcat
+echo $white"              ▒▒▒   ▒▒▒   ▒▒▒   ▒▒▒   ▒▒▒   ▒▒▒   ▒▒▒      "|lolcat
+echo $red"           ▒▒▒   ▒▒▒   ▒▒▒   ▒▒▒   ▒▒▒   ▒▒▒   ▒▒▒           " |lolcat
+echo $white"              ▒▒▒   ▒▒▒   ▒▒▒   ▒▒▒   ▒▒▒   ▒▒▒   ▒▒▒      " |lolcat
+echo $red"           ▒▒▒   ▒▒▒   ▒▒▒   ▒▒▒   ▒▒▒   ▒▒▒   ▒▒▒      " |lolcat
+echo $white"              ▒▒▒   ▒▒▒   ▒▒▒   ▒▒▒   ▒▒▒   ▒▒▒   ▒▒▒   " |lolcat
+echo $red"           ▒▒▒   ▒▒▒   ▒▒▒   ▒▒▒   ▒▒▒   ▒▒▒   ▒▒▒       "|lolcat
+echo ""
+echo $red"●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●"
+echo ""
+echo $green"############################################################"| lolcat
+echo $yellow"●TYPE : ZIXFACE TOOLS $green                                     #" |lolcat
+echo $purple"●CREATED : Cyb3r_Z1Xx3r $green                                   #" |lolcat
+echo $blue"●THANKS TO ALL MEMBER UNDERGROUND CYBER ARMY $green              #" |lolcat
+echo $cyan"●MY TEAM : UNDERGROUND CYBER ARMY                          #" |lolcat
+echo $red"●CONTACT Cyber_Z1Xx3r : 081356467*** $green                      #" |lolcat
+echo $green"############################################################"| lolcat
+
+echo $red  "==========================================================="
+sleep 1
+###################################################
+# CTRL + C
+###################################################
+trap ctrl6_c INT
+ctrl_c() {
+clear
+echo $red"~~> oke,  ... "
+echo $cyan".."
+sleep 1
+echo ""
+echo -e $r"..."
+sleep 1
+}
+
+lagi=1
+while [ $lagi -lt 20 ];
+do
+
+sleep 1
+echo $red"༎༎༎༎SILAHKAN PILIH TOOLS PENGEN ANDA PAKE༎༎༎༎ "
+sleep 2
+echo $red"+++++++++++++++++++++++++++++++++++++++++++++ "
+echo $cyan"[ 1] PHISING V1${enda}";
+echo $red"+++++++++++++++++++++++++++++++++++++++++++++ "
+echo $yellow"[ 2] PHISING V2${endc}";
+echo $red"+++++++++++++++++++++++++++++++++++++++++++++ "
+echo $purple"[ 3] PHISING V3${endc}";
+echo $red"+++++++++++++++++++++++++++++++++++++++++++++ "
+echo $white"[ 4] PHISING V4${endc}";
+echo $red"+++++++++++++++++++++++++++++++++++++++++++++ "
+echo $blue"[ 5] PHISING GAME${endc}";
+echo $red"+++++++++++++++++++++++++++++++++++++++++++++ "
+echo $green"[ 6] Hack fb target${endc}";
+echo $red"+++++++++++++++++++++++++++++++++++++++++++++ "
+echo $cyan"[ 7] Hack fb massal";
+echo $red"+++++++++++++++++++++++++++++++++++++++++++++ "
+echo $blue"[ 8] Hack fb Target+Massal";
+echo $red"+++++++++++++++++++++++++++++++++++++++++++++ "
+echo $green"[ 9] Hack FB ans    (#root)";
+echo $red"+++++++++++++++++++++++++++++++++++++++++++++ "
+echo $yellow"[10] Hack Instagram (#root)";
+echo $red"+++++++++++++++++++++++++++++++++++++++++++++ "
+echo $white"[11] Hack Twitter   (#root)";
+echo $red"+++++++++++++++++++++++++++++++++++++++++++++ "
+echo $purple"[12] Hack Gmail     (#root)";
+echo $red"+++++++++++++++++++++++++++++++++++++++++++++ "
+echo $green"[13] Fb Info";
+echo $red"+++++++++++++++++++++++++++++++++++++++++++++ "
+echo $cyan"[14] Santet Online";
+echo $red"+++++++++++++++++++++++++++++++++++++++++++++ "
+echo $blue"[15] Spam IG";
+echo $red"+++++++++++++++++++++++++++++++++++++++++++++ "
+echo $cyan"[16] Spam WA";
+echo $red"+++++++++++++++++++++++++++++++++++++++++++++ "
+echo $white"[17] Spam Sms";
+echo $red"+++++++++++++++++++++++++++++++++++++++++++++ "
+echo $green"[18] Youtube AutoView (#root)";
+echo $red"+++++++++++++++++++++++++++++++++++++++++++++ "
+echo $cyan"[19] Tembak Kuota XL";
+echo $red"+++++++++++++++++++++++++++++++++++++++++++++ "
+echo $white"[20] Tembak Cewe(khusus jones :v)";
+echo $red"+++++++++++++++++++++++++++++++++++++++++++++ "
+echo $blue"[ 0] Modar/Exit";
+echo $red"+++++++++++++++++++++++++++++++++++++++++++++ "
+echo ""
+echo $red "Masukan Nomornya :)"
+echo $red "ketik (TT) untuk cara guna/tutor  "
+read -p "====>" pil  ;
+figlet Cyb3r_Z1Xx3r | lolcat
+#phs shelpis
+
+case $pil in
+1)apt update
+apt upgrade
+git clone https://github.com/thelinuxchoice/shellphish.git
+cd shellphish
+bash shellphish.sh
+
+;;
+
+# phs be
+
+2) apt update
+apt upgrade
+git clone https://github.com/thelinuxchoice/blackeye.git
+cd blackeye
+bash blackeye.sh
+
+;;
+
+#phs socialfis
+
+3) apt update
+apt upgrade
+git clone https://github.com/UndeadSec/SocialFish.git
+cd SocialFish
+chmod +x *
+pip2 install -r requirements.txt
+python2 SocialFish.py
+
+;;
+
+#phs weeman
+
+4) git clone
+https://github.com/evait-security/weeman.git
+chmod +x *
+python2 weeman.py
+
+;;
+
+#hack ig
+
+10) git clone
+https://github.com/thelinuxchoice/instashell.git
+cd instashell
+bash instashell.sh
+sleep 1
+echo "root dulu"
+
+;;
+
+15) apt update
+apt upgrade
+apt install git
+git clone
+https://github.com/thelinuxchoice/instaspam.git
+ls
+cd instaspam
+bash instaspam.sh
+
+;;
+
+20)echo "lu Jomblo? Santai Bro.."
+echo "Kontol Masih Banyak, Jadi Homo Aja ;v"
+
+;;
+
+11)git clone https://github.com/thelinuxchoice/tweetshell
+cd tweetshell
+chmod +x tweetshell.sh
+bash tweetshell.sh
+sleep 2
+echo -e $g "TOOLS INI BUTUH AKSES ROOT"
+
+;;
+
+12) apt update && apt upgrade
+git clone https://github.com/thelinuxchoice/gmailshell.git
+cd gmailshell
+bash gmailshell.sh
+sleep 1
+echo -e $g"root dulu, baru bisa jalan tod"
+
+;;
+
+9)apt update && apt upgrade
+git clone https://github.com/thelinuxchoice/facebash.git
+cd facebash
+bash facebash.sh
+sleep 2
+echo -e $g"TOOLS INI BUTUH AKSES ROOT "
+
+;;
+
+7) pip2 install mechanize
+pip2 install requests
+pip2 install --upgrade pip
+git clone https://github.com/alpian9890/bruteforce-hack-fb.git
+cd bruteforce-hack-fb.git
+python2 MBF.py
+
+;;
+
+6) pip2 install --upgrade pip
+git clone https://github.com/Senitopeng/fbbrute.git
+cd fbbrute
+python2 jomblo.py
+
+;;
+
+8) pip2 install --upgrade pip
+pip2 install mechanize
+pip2 install requests
+git clone https://github.com/Senitopeng/KumpulanFbbrute.git
+cd KumpulanFbbrute
+python2 jomblo.py
+
+;;
+
+19) pip2 install requests
+pkg install python
+pkg install git
+pip install requests
+git clone https://github.com/albertoanggi/xl-py.git
+cd xl-py
+python app.py
+
+;;
+
+17) pip2 install requests
+ apt install nano
+ apt install git
+ git clone https://github.com/Senitopeng/SpamSms.git
+ cd SpamSms
+ chmod +x mantan.py
+ python2 mantan.py
+
+;;
+
+16) apt update -y
+apt upgrade -y
+apt install git
+apt install php
+git clone https://github.com/siputra12/prank
+cd prank
+ls
+php wa.php
+
+;;
+
+13) git clone https://github.com/CiKu370/OSIF.git
+cd OSIF
+python2 osif.py
+
+;;
+
+14)pkg install python
+git clone
+https://github.com/Gameye98/santet-online.git
+cd santet-online
+python santet.py
+
+;;
+
+18)apt update
+apt upgrade
+apt install python
+apt install git
+git clone https://github.com/thelinuxchoice/youbot.git
+cd youbot
+service tor start
+sudo ./youbot.sh URL
+
+;;
+
+5) apt update && apt upgrade
+apt install python2
+apt install apache2
+apt install git php unzip
+git clone https://github.com/Senitopeng/PhisingOnline.git
+cd PhisingOnline
+ls
+unzip PhisingOnline.zip
+python2 online.py
+
+;;
+
+0)figlet "THANK'S"|lolcat
+sleep 2
+exit
+;;
+
+*) echo "NOMORNYA GA ADA DI DAFTAR TOD !!"  | lolcat
+esac
+done
+done
